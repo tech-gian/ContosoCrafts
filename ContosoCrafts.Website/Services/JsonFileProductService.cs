c20df2c5f120479187001a8d470326dde909653e
Giannis Zapantis <sdi1900059@di.uoa.gr>
2021-09-09 12:56:08 +0300

    Modify, service

--- ContosoCrafts.Website/Services/JsonFileProductService.cs
+++ ContosoCrafts.Website/Services/JsonFileProductService.cs
@@ -34,5 +34,36 @@ namespace ContosoCrafts.Website.Services
                     });
             }
         }
+
+        public void AddRating(string productId, int rating)
+        {
+            var products = GetProducts();
+
+            // LINQ
+            var query = products.First(x => x.Id == productId);
+            
+            if (query.Ratings == null)
+            {
+                query.Ratings = new int[] { rating };
+            }
+            else
+            {
+                var ratings = query.Ratings.ToList();
+                ratings.Add(rating);
+                query.Ratings = ratings.ToArray();
+            }
+
+            using (var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<Product>> (
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    products
+                );
+            }
+        }
     }
 }
